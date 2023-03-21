69d983e35305e27a0043f4fcc0dd6d96ca9dd11a
keakon <keakon@yizhisec.com>
2023-03-21 10:02:47 +0800

    test ci

--- examples/caller.py
+++ examples/caller.py
@@ -3,7 +3,7 @@
 from delayed.task import Task
 
 from .client import queue
-from .tasks import func1, func2, error_handler, DELAY, DELAY_WITH_PARAMS
+from .tasks import func1, func2, DELAY
 
 
 DELAY(func1)(1, 2, x=3)
@@ -11,10 +11,8 @@ DELAY(func1)(1, 2, x=3)
 func2(1, 2, x=3)
 func2.delay(1, 2, x=3)
 
-DELAY_WITH_PARAMS(timeout=0.1, error_handler=error_handler)(func1)(1, 2, x=3)
-
-task = Task(id=None, func_path='examples.tasks:func1', args=(1, 2), kwargs={'x': 3}, timeout=0.1, error_handler_path='examples.tasks:error_handler')
+task = Task(id=None, func_path='examples.tasks:func1', args=(1, 2), kwargs={'x': 3})
 queue.enqueue(task)
 
-task = Task.create(func=func1, args=(1, 2), kwargs={'x': 3}, timeout=0.1, prior=True, error_handler=error_handler)
+task = Task.create(func=func1, args=(1, 2), kwargs={'x': 3})
 queue.enqueue(task)
--- examples/worker.py
+++ examples/worker.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from delayed.logger import setup_logger
-from delayed.worker import PreforkedWorker
+from delayed.worker import Worker
 
 from .client import queue
 
 
 setup_logger()
 
-worker = PreforkedWorker(queue=queue)
+worker = Worker(queue=queue)
 worker.run()
